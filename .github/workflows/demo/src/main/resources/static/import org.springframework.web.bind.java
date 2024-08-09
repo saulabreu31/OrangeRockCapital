f1f8b7863d9ea213886c010c61fc753a36cb4ccc
saulabreu31 <53130862+saulabreu31@users.noreply.github.com>
2024-08-08 20:21:48 -0400

    ccc

---
+++ .github/workflows/demo/src/main/resources/static/import org.springframework.web.bind.java
@@ -0,0 +1,11 @@
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "This is the index page!";
+    }
+}
