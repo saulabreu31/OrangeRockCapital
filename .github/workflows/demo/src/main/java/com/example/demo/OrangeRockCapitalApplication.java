ae1623ec3011fbb130c17661975e15396d60ecb8
saulabreu31 <53130862+saulabreu31@users.noreply.github.com>
2024-09-02 20:45:01 -0400

    change

---
+++ .github/workflows/demo/src/main/java/com/example/demo/OrangeRockCapitalApplication.java
@@ -0,0 +1,21 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class OrangeRockCapitalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrangeRockCapitalApplication.class, args);
+    }
+}
+
+@RestController
+class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Welcome to Orange Rock Capital!";
+    }
+}
+
+
