5c4df2b1bf694bb1f0d845fdab74b0868bfe18f2
saulabreu31 <53130862+saulabreu31@users.noreply.github.com>
2024-10-04 20:53:15 -0400

    COMMIT

--- .github/workflows/demo/src/main/java/com/example/demo/OrangeRockCapitalApplication.java
+++ .github/workflows/demo/src/main/java/com/example/demo/OrangeRockCapitalApplication.java
@@ -1,3 +1,4 @@
+package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +12,7 @@ public class OrangeRockCapitalApplication {
 }
 
 @RestController
-class HelloController {
+class WelcomeController {
     @GetMapping("/")
     public String hello() {
         return "Welcome to Orange Rock Capital!";
